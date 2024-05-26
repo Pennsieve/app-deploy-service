d5bb737ec387347404356c35f7b2a3c40d502dcd
Edmore Moyo <etmoyo@gmail.com>
2024-05-26 16:15:45 -0400

    app specific roles and policies

--- fargate/app-provisioner/terraform/infrastructure/iam.tf
+++ fargate/app-provisioner/terraform/infrastructure/iam.tf
@@ -1,13 +1,13 @@
 // ## App ##
 resource "aws_iam_role" "task_role_for_app" { #
-  name               = "task_role_for_app-${var.account_id}-${var.env}"
+  name               = "task_role_for_${var.app_slug}-${var.account_id}-${var.env}"
   assume_role_policy = data.aws_iam_policy_document.app_role_assume_role.json
   managed_policy_arns = [aws_iam_policy.app_efs_policy.arn]
 }
 
 # TODO: resource should be specific EFS ID
 resource "aws_iam_policy" "app_efs_policy" { #
-  name = "app_role_efs_policy-${var.account_id}-${var.env}"
+  name = "app_role_efs_policy-${var.app_slug}-${var.account_id}-${var.env}"
 
   policy = jsonencode({
     Version = "2012-10-17"
@@ -40,13 +40,13 @@ data "aws_iam_policy_document" "app_role_assume_role" { #
 
 // ECS Task Execution IAM role
 resource "aws_iam_role" "execution_role_for_app" { #
-  name               = "execution_role_for_app-${var.account_id}-${var.env}"
+  name               = "execution_role_for_${var.app_slug}-${var.account_id}-${var.env}"
   assume_role_policy = data.aws_iam_policy_document.app_execution_role_assume_role.json
   managed_policy_arns = [aws_iam_policy.app_execution_role_policy.arn]
 }
 
 resource "aws_iam_policy" "app_execution_role_policy" { #
-  name = "app_task_execution_role_policy-${var.account_id}-${var.env}"
+  name = "${var.app_slug}_task_execution_role_policy-${var.account_id}-${var.env}"
 
   policy = jsonencode({
     Version = "2012-10-17"
