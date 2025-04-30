5fb6fc7d3c625f57e797cff0f94842544a6a2a07
Edmore Moyo <etmoyo@gmail.com>
2025-04-30 15:55:28 -0400

    use known attribute

--- fargate/app-provisioner/terraform/pennsieve/public-repository/outputs.tf
+++ fargate/app-provisioner/terraform/pennsieve/public-repository/outputs.tf
@@ -1,5 +1,5 @@
 output "app_public_ecr_repository" {
   description = "App Public ECR repository"
 
-  value = aws_ecrpublic_repository.public_repo.repository_url
+  value = aws_ecrpublic_repository.public_repo.repository_uri
 }
