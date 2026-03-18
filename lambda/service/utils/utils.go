a268e33264ffaf2b208923e6aed8714c9d6c823d
Edmore Moyo <etmoyo@gmail.com>
2026-03-18 12:10:16 -0400

    reverts changes

--- lambda/service/utils/utils.go
+++ lambda/service/utils/utils.go
@@ -7,9 +7,6 @@ import (
 func ExtractRoute(requestRouteKey string) string {
 	r := regexp.MustCompile(`(?P<method>) (?P<pathKey>.*)`)
 	routeKeyParts := r.FindStringSubmatch(requestRouteKey)
-	if len(routeKeyParts) == 0 {
-		return "/"
-	}
 	pathKey := routeKeyParts[r.SubexpIndex("pathKey")]
 	if pathKey == "" {
 		return "/"
